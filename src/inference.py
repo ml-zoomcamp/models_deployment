6b8349f4919cdc46cc2bb63b04a332cdfe36bea7
Ivan <ivmalyutinhse@gmail.com>
2024-10-28 13:28:28 +0400

    Add model inference script

---
+++ src/inference.py
@@ -0,0 +1,29 @@
+import pickle
+
+class Model_loader:
+    def __init__(self, path_to_dv, path_to_lr):
+
+        # load DictVectorizer
+        with open(path_to_dv, 'rb') as dv_file: 
+            self.dv = pickle.load(dv_file)
+
+        # load LogisticRegression
+        with open(path_to_lr, 'rb') as lr_file: 
+            self.lr = pickle.load(lr_file)
+        
+    def forward(self, client): 
+        X = self.dv.transform(client)
+        prob = self.lr.predict_proba(X)[0][1]
+
+        return prob
+
+if __name__ == "__main__": 
+    path_to_dv = "models/dv.bin"
+    path_to_lr = "models/model1.bin"
+    loader = Model_loader(path_to_dv, path_to_lr)
+    
+    """ Make prediction on client examples
+    """
+    client_example = {"job": "management", "duration": 400, "poutcome": "success"}
+    prob = loader.forward(client_example)
+    print(f"Prob to get a subscription: {prob:.3f}")
