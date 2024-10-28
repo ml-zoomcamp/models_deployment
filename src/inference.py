eb8d37cc6e724013f29efb16aa7e72b93adc6a1e
Ivan <ivmalyutinhse@gmail.com>
2024-10-28 20:41:46 +0400

    Update inference.py

--- src/inference.py
+++ src/inference.py
@@ -1,7 +1,10 @@
 import pickle
+import os
 
 class Model_loader:
-    def __init__(self, path_to_dv, path_to_lr):
+    def __init__(self,
+                    path_to_dv:os.PathLike = "models/dv.bin",
+                        path_to_lr:os.PathLike = "models/model1.bin"):
 
         # load DictVectorizer
         with open(path_to_dv, 'rb') as dv_file: 
@@ -11,18 +14,19 @@ class Model_loader:
         with open(path_to_lr, 'rb') as lr_file: 
             self.lr = pickle.load(lr_file)
         
-    def forward(self, client): 
+    def forward(self, client: dict): 
+        # make predictions
         X = self.dv.transform(client)
         prob = self.lr.predict_proba(X)[0][1]
 
-        return prob
+        return round(prob, 3)
 
 if __name__ == "__main__": 
     path_to_dv = "models/dv.bin"
     path_to_lr = "models/model1.bin"
     loader = Model_loader(path_to_dv, path_to_lr)
     
-    """ Make prediction on client examples
+    """ Make prediction on client example
     """
     client_example = {"job": "management", "duration": 400, "poutcome": "success"}
     prob = loader.forward(client_example)
